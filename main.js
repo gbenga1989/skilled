4065c619558f0dbed02ff8ec77d71c141d9e1723
gbenga <odetoyinbog@gmail.com>
2025-01-29 10:30:27 +0100

    28

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ ScrollReveal().reveal(".hero-content h1", {
   ...scrollRevealOption,
   origin: "right",
 });
+
+ScrollReveal().reveal(".course-box", {
+  ...scrollRevealOption,
+  origin: "bottom",
+  delay: 500,
+});
